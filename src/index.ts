7a2dd566c42a8e493fb4e0e9cd5e564b87a74989
aequi42 <aequi42@users.noreply.github.com>
2021-07-10 18:19:44 +0200

    Add multiple reactions

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
+import { config } from 'dotenv'
 import { Telegraf } from 'telegraf'
+import { getRandomReaction } from './reactions'
+
+config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN!)
 // bot.start((ctx) => ctx.reply('Welcome'))
 // bot.help((ctx) => ctx.reply('Send me a sticker'))
 // bot.on('sticker', (ctx) => ctx.reply(`👍 ${JSON.stringify(ctx.message,null,2)}`))
 // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.hears(/ekbaris/i, ctx => ctx.replyWithSticker("CAACAgIAAxkBAAMNYMHlHc-b-ReJNrWDf9tOfd9Fh_4AAgEAAzuZXCI0ZkH_YXJe6R8E"))
-bot.hears(/elthiadore/i, ctx => ctx.replyWithSticker("CAACAgIAAxkBAAMPYMHlOlFJKZ-Cq74dtBTTe1PrPdsAAgIAAzuZXCL0SAAB0cuhsm8fBA"))
-bot.hears(/maximus/i, ctx => ctx.replyWithSticker("CAACAgIAAxkBAAMUYMHnOAQ1yD_BJ3ttiDgerkf1pCoAAgMAAzuZXCJYUlUjN1zsbh8E"))
-bot.hears(/balos/i, ctx => ctx.replyWithSticker("CAACAgIAAxkBAAMXYMHnXfKijRqQTrny_7jFsmIheDYAAgQAAzuZXCJe5pwyFU2TER8E"))
+bot.hears(/ekbaris/i, getRandomReaction("ekbaris"))
+bot.hears(/elthiadore/i, getRandomReaction("elthiadore"))
+bot.hears(/maximus/i, getRandomReaction("elthiadore"))
+bot.hears(/balos/i, getRandomReaction("balos"))
+bot.hears(/vhucthul/i, getRandomReaction("vhucthul"))
+bot.hears(/zork/i, getRandomReaction("zork"))
 bot.launch()
 
 // Enable graceful stop
---
+++ src/reactions.ts
@@ -0,0 +1,40 @@
+import type { Context } from "telegraf"
+import { getRandomIntInclusive } from "./utilities"
+
+
+export const Reactions = {
+  zork: [
+    (ctx: Context)=> ctx.reply(`${ctx.from?.username}, du Made wagst es, meinen Namen auszusprechen! Der Tod soll dich holen!`),
+    (ctx: Context) => ctx.reply("Schweigt still! Ich werde euch vernichten!"),
+    (ctx: Context) => ctx.reply("Die Herren der Drachen werden Untergehen!"),
+    (ctx: Context) => ctx.replyWithAnimation("https://media.giphy.com/media/lcFSmTvaSxwSk/giphy.gif"),
+    (ctx: Context) => ctx.reply("😈")
+  ],
+  ekbaris: [
+    (ctx: Context) => ctx.reply(`Ekbaris? Dieser mikriger Magier? Sein Drache soll ihn Fressen!`),
+    (ctx: Context) => ctx.replyWithSticker("CAACAgIAAxkBAAMNYMHlHc-b-ReJNrWDf9tOfd9Fh_4AAgEAAzuZXCI0ZkH_YXJe6R8E"),
+  ],
+  elthiadore: [
+    (ctx: Context) => ctx.reply(`Pha! Elthiadóre kann mir garnichts! Soll er lieber bei seinen Büchern bleiben!`),
+    (ctx: Context) => ctx.replyWithSticker("CAACAgIAAxkBAAMPYMHlOlFJKZ-Cq74dtBTTe1PrPdsAAgIAAzuZXCL0SAAB0cuhsm8fBA"),
+  ],
+  maximus: [
+    (ctx: Context) => ctx.replyWithSticker("CAACAgIAAxkBAAMUYMHnOAQ1yD_BJ3ttiDgerkf1pCoAAgMAAzuZXCJYUlUjN1zsbh8E")
+  ],
+  balos: [
+    (ctx:Context) => ctx.replyWithSticker("CAACAgIAAxkBAAMXYMHnXfKijRqQTrny_7jFsmIheDYAAgQAAzuZXCJe5pwyFU2TER8E")
+  ],
+  vhucthul: [
+    (ctx:Context) => ctx.reply("Argh! Dieser Verräter!")
+  ]
+}
+
+
+export function getRandomReaction(trigger: keyof typeof Reactions) {
+  const values = Reactions[trigger]
+  const length = values.length
+  return (ctx: Context) => {
+    var idx = getRandomIntInclusive(0,length-1)
+    return values[idx](ctx)
+  };
+}
